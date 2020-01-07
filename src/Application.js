53f3b3f6ade79aa382cc824b29375ff1ccbcf1db
huewilliams <4879sinwoo@naver.com>
2020-01-07 11:13:56 +0900

    ADD: 정적 파일 제공 기능 구현(mimeType, readFile 설정)

--- src/Application.js
+++ src/Application.js
@@ -1,18 +1,45 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const Application = () => {
-    const _server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World\n');
+    const server = http.createServer((req, res) => {
+
+        const mimeType = {
+            '.ico': 'image/x-icon',
+            '.html': 'text/html',
+            '.js': 'text/javascript',
+            '.css': 'text/css',
+            '.png': 'image/png',
+            '.jpg': 'image/jpeg',
+            '.eot': 'appliaction/vnd.ms-fontobject',
+            '.ttf': 'aplication/font-sfnt'
+        }
+        const ext = path.parse(req.url).ext;
+        const filePath = path.join(__dirname, `../public/${req.url}`);
+
+        if(Object.keys(mimeType).includes(ext)) {
+            fs.readFile(filePath, (err, data)=> {
+                if (err) {
+                    res.statusCode = 404;
+                    res.end('Not Found');
+                } else {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', mimeType[ext]);
+                    res.end(data);
+                }
+            })   
+        } else {
+            res.status(200);
+        }
     })
 
     const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
-        _server.listen(port, hostname, fn);
+        server.listen(port, hostname, fn);
     }
 
     return {
-        _server,
+        server,
         listen
     }
 }
