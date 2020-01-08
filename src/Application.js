7c01b77a7c605ef0db51612e87d009afab639b95
huewilliams <4879sinwoo@naver.com>
2020-01-08 15:34:41 +0900

    FIX: 정적 파일 처리 로직 Serve-static 모듈 사용

--- src/Application.js
+++ src/Application.js
@@ -1,38 +1,21 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const serveStatic = require('./serve-static');
 
 const Application = () => {
     const server = http.createServer((req, res) => {
+        serveStatic(req, res);
 
-        const mimeType = {
-            '.ico': 'image/x-icon',
-            '.html': 'text/html',
-            '.js': 'text/javascript',
-            '.css': 'text/css',
-            '.png': 'image/png',
-            '.jpg': 'image/jpeg',
-            '.eot': 'appliaction/vnd.ms-fontobject',
-            '.ttf': 'aplication/font-sfnt'
-        }
-        const ext = path.parse(req.url).ext;
-        const filePath = path.join(__dirname, `../public/${req.url}`);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
 
-        if(Object.keys(mimeType).includes(ext)) {
-            fs.readFile(filePath, (err, data)=> {
-                if (err) {
-                    res.statusCode = 404;
-                    res.end('Not Found');
-                } else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', mimeType[ext]);
-                    res.end(data);
-                }
-            })   
-        } else {
-            res.status(200);
-        }
-    })
+        const filePath = path.join(__dirname, '../public/index.html');
+        fs.readFile(filePath, (err, data) =>{
+            if (err) throw err;
+            res.end(data);
+        });
+    });
 
     const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
         server.listen(port, hostname, fn);
